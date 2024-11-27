94374a8fe0612ef020250110e3019a772e8c80e6
Виктор <imperia.zt@gmail.com>
2024-11-27 10:17:47 +0200

    upload dont work

--- components/oneSetPage/styles.oneSetPage.ts
+++ components/oneSetPage/styles.oneSetPage.ts
@@ -71,7 +71,7 @@ const stylesOneSet = StyleSheet.create({
   buttonbox: {
     textAlign: "center",
     justifyContent: "center",
-    width: 80,
+    width: 70,
     height: 40,
     borderRadius: 7,
     backgroundColor: colors.dark,
@@ -156,6 +156,11 @@ const stylesOneSet = StyleSheet.create({
     paddingBottom: 0,
     fontSize: 16,
     zIndex: 2,
+  },
+  linkUpload: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
   },
 })
 
